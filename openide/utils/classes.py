8ac7f42876734341e21f8831854d0b9acbf1fd63
Axel Voitier <axel.voitier@gmail.com>
2023-02-12 13:49:38 +0100

    Utils: Actually really fixes MetaClassResolver for PySide6
    Turns out previous solution was broken. As we were intercepting the __new__ call before it reached type(QObject), and replacing the class argument to swap it with another one, it turns out this new class became the metaclass of the generated class.
    
    So, it seemed to work fine for the class we were generating. But any subclass of that one was being created using the "wrong" (ie. incomplete) metaclass. And so the original metaclasses behaviour (eg. ABCMeta, SingletongMeta) were not honored for subclasses...
    
    New solution seems to fixes it all. And even seems to be less hacky (but explanation for it is much more obscur though!).

--- openide/utils/classes.py
+++ openide/utils/classes.py
@@ -13,7 +13,6 @@ from abc import ABCMeta
 from functools import lru_cache
 
 # Third-party imports
-from qtpy.QtCore import QObject
 
 # Local imports
 
@@ -57,9 +56,8 @@ def MetaClassResolver(*subclasses, extra_metas=None):
     class MyClass(MetaClassResolver(extra_metas=[ABCMeta, SingletonMeta])):
         ...
     '''
-    # Main principle of a metaclass resolver is to generate
-    # a dynamic metaclass subclassing all metaclasses of the subclasses
-    # we are interested to have.
+    # Main principle of a metaclass resolver is to generate a dynamic metaclass
+    # subclassing all metaclasses of the subclasses we are interested to have.
     #
     # class _ResolverMeta(metaclass1, metaclass2, ...): ...
     # class _Resolver(subclass1, subclass2, ..., metaclass=_ResolverMeta): ...
@@ -76,81 +74,105 @@ def MetaClassResolver(*subclasses, extra_metas=None):
         all_metas.remove(type)
     all_metas.append(type)
 
-    qobj_tp = type(QObject)
-    if qobj_tp in all_metas:
-        # Solves for PySide6 no longer accepting easily to derive
-        # metaclasses from their own metaclass.
-        # See PYSIDE-1434 and PYSIDE-1767
+    # Fix for PySide6 QObject inheritance (would probably also work for other "C base metatype"))
+    # That one was very tricky to figure out...
+    #
+    # If you do a simple "class _ResolverMeta(ABCMeta, type(QObject))", you will have an error like
+    # "TypeError: Shiboken.ObjectType.__new__(_ResolverMeta) is not safe, use type.__new__()" when
+    # trying to construct a class with this metaclass. See PYSIDE-1434 and PYSIDE-1767.
+    #
+    # In a glimpse, it looks like if you revert the order of the subclasses of _ResolverMeta, it
+    # seems to be working. But actually, because ATM PySide does not support cooperative multiple
+    # inheritence (ie. won't call super), ABCMeta.__new__ is never called, and the
+    # __abstractmethods__ attribute on the class is never set. And if that was another metaclass
+    # than ABCMeta, it would be the same, its __new__ would never be executed.
+    # And in that case, we cannot make _ResolverMeta.__new__ explicitly call each subclass __new__,
+    # like you would in an __init__ in a diamond inheritence. Because each __new__ will return you a
+    # different instance, when you want a unique one.
+    #
+    # The (cryptic) "is not safe" error comes from a check on Python side, from Objects/typeobject.c
+    # in tp_new_wrapper function. It tries to check that the tp_new of a type is the same than the
+    # tp_new of the "most derived base that's not a heap type". Also very cryptic...
+    # Note that PySide "recently" switched to use Python Limited API, meaning using heap types.
+    # That might be why it used to work easily on PySide2.
+    #
+    # Another thing that does not help at first (but was actually key to lead me to the solution),
+    # is if you make the code of that function correspond to the error message it generates, it
+    # looks like "the most derived base that's not a heap type" it finds (ie. "staticbase") seems
+    # to be "type" itself. But it is not. If you experiment a bit with the MRO of _ResolverMeta,
+    # you can find with this:
+    # > class IntermediateType(type): ...
+    # > class _ResolverMeta(IntermediateType, ABCMeta, type(QObject)): ...
+    # You will get:
+    # Shiboken.ObjectType.__new__(_ResolverMeta) is not safe, use IntermediateType.__new__()
+    # Note how "type" became "IntermediateType" in the error message.
+    #
+    # However, if you just add a passthrough __new__ method to IntermediateType:
+    # > class IntermediateType(type):
+    # >     def __new__(mcls, name, bases, namespace, /, **kwargs):
+    # >         return super().__new__(mcls, name, bases, namespace, **kwargs)
+    # > class _ResolverMeta(IntermediateType, ABCMeta, type(QObject)): ...
+    # Then you get the error message mentioning "type" again:
+    # Shiboken.ObjectType.__new__(_ResolverMeta) is not safe, use type.__new__()
+    #
+    # The reasons why when you reimplement __new__ you get an incorrect class name when it finds the
+    # "staticbase" are still unclear. But at least that test confirms it actually choke on the
+    # "meta-type" of the first "meta-subclass".
+    #
+    # And so, to bypass that "safe" check, we want to trick it to think the first meta-subclass is
+    # of the same meta-type than the one we will end up calling when we reach the type(QObject) one
+    # down the MRO of _ResolverMeta.
+    # And one way to do that is to have that IntermediateType class above actually "subclass"
+    # type(QObject) instead of type. As we have another type(QObject) further down in the MRO, on
+    # one side, during normal processing (think executing the chain of __new__ through the MRO) it
+    # will just "passthrough" that do-nothing IntermediateType, and correctly execute
+    # ABCMeta.__new__ before "terminating" in that non-cooperative type(QObject).__new__.
+    # And on the other side, when we get to execute type(QObject).__new__, the "safe" check will
+    # think "the most derived base that's not a heap type" is IntermediateType, which now happens to
+    # have the same tp_new than type(QObject).
+    #
+    # To make that a bit more generic (the same issue and solution would apply for any other C-based
+    # metatype that is a heap type), we can actually collect all types of the metatypes we intend
+    # to have. And if we have something else than just "type", then add that IntermediateType (that
+    # we call _MetaTypeFence below), making it subclass those other metatype types.
+    # Actually, in case we find more than one "exotic" metatype type (Qt stuffs + some other C-based
+    # objects), the behaviour is yet to be defined...
+    all_meta_types = list({type(meta): meta for meta in all_metas if type(meta) is not type}.keys())
+    if all_meta_types:
+        class _MetaTypeFence(*all_meta_types):
+            pass
 
-        # First, we remove the QObject type from list of metas
-        # because we will be manipulating how it finds it.
-        # That also means with this solution, the type(QObject) can't be in one
-        # particular, user-defined order in the metaclass MRO.
-        # But usually that's fine for most cases.
-        all_metas.remove(qobj_tp)
+        all_metas.insert(0, _MetaTypeFence)
 
-        # We construct a class that starts with the QObject type
-        # and then list all the other metas.
-        # When you instantiate such class, the __new__ of QObject type
-        # will be happy to be called as it expects it
-        # (ie. avoiding the "... is not safe, use type.__new__...").
-        # It will also solve the infamous "the metaclass of a derived class must be..."
-        # because the created class will indeed have a metaclass made of
-        # all the metaclasses of its subclasses.
-        # However, QObject type will not call super().__new__! So, other metaclasses
-        # after it never get to execute their new, if you only do that.
-        class _QObjectResolver(qobj_tp, *all_metas):
-            ...
-
-        # Next, to solve for the other metaclasses __new__ not being called,
-        # the solution is to have them be called first (ie. as if QObject type
-        # was last in the metaclass MRO). But then you have to intercept
-        # the last super().__new__ just before QObject type __new__ gets called
-        # (to avoid the "... is not safe, use type.__new__()").
-        # And instead swap it with a call to our _QObjectResolver above.
-        # The true object will effectively be generated by it. And it will be
-        # returned-percolated back up the chain of the other metaclasses __new__.
-        class _QObjectInterceptor(qobj_tp):
-            def __new__(cls, *args, **kwargs):
-                return super().__new__(_QObjectResolver, *args, **kwargs)
-
-        # Now we can register our __new__ interceptor as the (almost) last
-        # class in the list of metaclasses, and construct a metaclass resolver
-        # just like if it was a simple resolution case.
-        all_metas.insert(-1, _QObjectInterceptor)  # Insert before type-class
-
-        # The magic in this method is to execute both:
-        # - The other metaclasses __new__, such that their behaviour is kept (eg. ABCMeta init)
-        # - The QObject type __new__ as if it was alone (and be the one creating the real object)
-        # And still make Python believes the final "hybrid" metaclass is
-        # actually made up of all the subclasses metaclasses.
-
-        # One last trick below though...
-
+    # Generate a metaclass that mixes all metaclasses, and will fix the typical error
+    # "TypeError: metaclass conflict: the metaclass of a derived class
+    #  must be a (non-strict) subclass of the metaclasses of all its bases"
     class _ResolverMeta(*all_metas):
         pass
 
+    # Generate a class that can be simply inherited to:
+    # - Subclass all given subclasses
+    # - Mix and fix their metaclasses
     class _Resolver(*subclasses, metaclass=_ResolverMeta):
 
         def __new__(cls, *args, **kwargs):
             obj = super().__new__(cls, *args, **kwargs)
             if (ABCMeta in all_metas) and obj.__abstractmethods__:
-                # In case an ABC is used along a QObject, it turns out
-                # the new of QObject is not checking for abstract methods.
+                # In case an ABC is used along a QObject (for instance), it turns out
+                # the new of QObject type is not checking for abstract methods.
                 # Therefore, even if ABCMeta.__new__ has properly run and
                 # inited its internal states, the actual check for abstraction
                 # is not enforced.
-                # Therefore, we kind of replicate it here, raising the TypeError ourselves.
+                # We replicate it here, raising the TypeError ourselves.
                 # In the event we are an ABC but not a QObject, and we
                 # do have abstract methods, the normal abstraction check will happen
                 # before we reach this one.
+                s = 's' if len(obj.__abstractmethods__) > 1 else ''
                 raise TypeError(
                     f'Can\'t instantiate abstract class {cls.__name__} '
-                    f'with abstract methods {", ".join(obj.__abstractmethods__)}'
+                    f'with abstract method{s} {", ".join(obj.__abstractmethods__)}'
                 )
             return obj
-
-        pass
 
     return _Resolver
 
--- tests/utils/test_metaclass_resolver.py
+++ tests/utils/test_metaclass_resolver.py
@@ -7,7 +7,7 @@
 from __future__ import annotations
 
 # System imports
-from abc import ABC, abstractmethod
+from abc import ABCMeta, ABC, abstractmethod
 from collections.abc import Generator, Iterable
 from typing import Type
 
@@ -19,27 +19,40 @@ from qtpy.QtWidgets import QWidget, QApplication
 from openide.utils.classes import MetaClassResolver, SingletonMeta
 
 
-class Normal1:
+class Interface(ABC):
 
-    def method(self) -> bool:
+    @abstractmethod
+    def method1(self) -> bool:
+        '''method1 is meant to be implemented by one of the given subclasses'''
+        raise NotImplementedError()
+
+    @abstractmethod
+    def method2(self) -> bool:
+        '''method2 is meant to be implemented by the class using MetaClassResolver directly'''
+        raise NotImplementedError()
+
+    @abstractmethod
+    def method3(self) -> bool:
+        '''method3 is meant to be implemented by a subclass of the class using MetaClassResolver'''
+        raise NotImplementedError()
+
+
+class Normal1:
+    '''A bog-standard class'''
+
+    def method1(self) -> bool:
         return True
 
 
 class Normal2:
+    '''A second bog-standard class, to combine with another bog-standard'''
 
-    def method(self) -> bool:
+    def method1(self) -> bool:
         return True
 
 
 class ASingleton(metaclass=SingletonMeta):
     ...
-
-
-class Interface(ABC):
-
-    @abstractmethod
-    def method(self) -> bool:
-        raise NotImplementedError()
 
 
 class AWidget(QWidget):
@@ -54,74 +67,147 @@ def qapp() -> Generator[None, None, None]:
     del qapp
 
 
-@pytest.mark.parametrize(
-    'subclasses', [
-        # Single
-        (Normal1, ),
-        (ASingleton, ),
-        pytest.param((Interface, ), marks=pytest.mark.xfail(raises=TypeError)),
-        (AWidget, ),
+def check(cls: Type, subclasses: Iterable[Type], check_method3=False) -> None:
+    '''Reusable checking function'''
+    # You would think checking some of these stuffs are superfluous.
+    # But I have seen very weird behaviours during development,
+    # as we manipulate some fundamental stuffs about class and object creation.
+    # So, don't dare optimizing these checks...
 
-        # Combined
-        (Normal1, Normal2),
-        (Normal1, ASingleton),
-        (ASingleton, Normal1),
-        (Normal1, Interface),
-        (Normal1, AWidget),
-        (AWidget, Normal1),
-        pytest.param((Interface, ASingleton), marks=pytest.mark.xfail(raises=TypeError)),
-        pytest.param((ASingleton, Interface), marks=pytest.mark.xfail(raises=TypeError)),
-        pytest.param((Interface, AWidget), marks=pytest.mark.xfail(raises=TypeError)),
-        pytest.param((AWidget, Interface), marks=pytest.mark.xfail(raises=TypeError)),
-
-        (Normal1, ASingleton, Interface),
-        (Normal1, Interface, ASingleton),
-        (ASingleton, Normal1, Interface),
-
-        (Normal1, AWidget, Interface),
-        (Normal1, Interface, AWidget),
-        (AWidget, Normal1, Interface),
-
-        (Normal1, ASingleton, AWidget),
-        (Normal1, AWidget, ASingleton),
-        (ASingleton, Normal1, AWidget),
-        (ASingleton, AWidget, Normal1),
-        (AWidget, Normal1, ASingleton),
-        (AWidget, ASingleton, Normal1),
-
-        (Normal1, Interface, ASingleton, AWidget),
-        (Normal1, Interface, AWidget, ASingleton),
-        (ASingleton, Normal1, Interface, AWidget),
-        (AWidget, Normal1, Interface, ASingleton),
-        (ASingleton, AWidget, Normal1, Interface),
-        (AWidget, ASingleton, Normal1, Interface),
-        (Normal1, ASingleton, Interface, AWidget),
-        (Normal1, AWidget, Interface, ASingleton),
-        (ASingleton, Normal1, AWidget, Interface),
-        (AWidget, Normal1, ASingleton, Interface),
-    ]
-)
-def test_only_subclasses(subclasses: Iterable[Type]) -> None:
-    class ToTest(MetaClassResolver(*subclasses)):  # type: ignore[misc]
-        ...
-
-    print(ToTest.__mro__)
-
-    obj = ToTest()
+    # An abstract class should not even instantiate
+    obj = cls()
     assert obj
-    if hasattr(obj, 'method'):
-        assert obj.method() is True
 
-    mro_iter = iter(ToTest.__mro__)
-    meta_mro = type(ToTest).__mro__
+    # If we are supposed to have a method1, check we are not hitting the abstract one
+    if hasattr(obj, 'method1'):
+        assert obj.method1() is True
+
+    # Method2 should always be here, and should not hit an abstract one
+    assert obj.method2() is True
+
+    # Method3 is like method2, but for a subclass test
+    if check_method3:
+        assert obj.method3() is True
+
+    # Checking some meta behaviours have run properly.
+    metas = {type(subclass) for subclass in subclasses}
+
+    # An ABC should have __abstractmethods__ (even if empty).
+    if ABCMeta in metas:
+        assert hasattr(cls, '__abstractmethods__') is True, 'It seems ABCMeta.__new__ did not run'
+        assert hasattr(obj, '__abstractmethods__') is True, 'It seems ABCMeta.__new__ did not run'
+
+    # A Singleton should be registered in the instances dict
+    if SingletonMeta in metas:
+        assert cls in SingletonMeta._instances
+        assert SingletonMeta._instances[cls] is obj
+
+    # Checking MRO of both the class and metaclass behave sanely
+    mro_iter = iter(cls.__mro__)
+    meta_mro = type(cls).__mro__
     for subclass in subclasses:
-        assert issubclass(ToTest, subclass)
+        # Typing tests should still work
+        assert issubclass(cls, subclass)
         assert isinstance(obj, subclass)
 
+        # Check we do find the subclass in the MRO, in the expected order
         for mro_class in mro_iter:
             if mro_class is subclass:
                 break
         else:
-            raise AssertionError(f'Subclass {subclass} not in MRO: {ToTest.__mro__}')
+            raise AssertionError(f'Subclass {subclass} not in MRO: {cls.__mro__}')
 
+        # Check the metaclass of a subclass is in the metaclass MRO
         assert type(subclass) in meta_mro, f'{type(subclass)} not in metaclass MRO: {meta_mro}'
+
+
+SUBCLASSES_TESTS = [
+    # Single
+    (Normal1, ),
+    (ASingleton, ),
+    pytest.param((Interface, ), marks=pytest.mark.xfail(raises=TypeError, strict=True)),
+    (AWidget, ),
+
+    # Combined
+    (Normal1, Normal2),
+    (Normal1, ASingleton),
+    (ASingleton, Normal1),
+    (Normal1, Interface),
+    (Normal1, AWidget),
+    (AWidget, Normal1),
+    pytest.param((Interface, ASingleton), marks=pytest.mark.xfail(
+        raises=TypeError, strict=True)),
+    pytest.param((ASingleton, Interface), marks=pytest.mark.xfail(
+        raises=TypeError, strict=True)),
+    pytest.param((Interface, AWidget), marks=pytest.mark.xfail(raises=TypeError, strict=True)),
+    pytest.param((AWidget, Interface), marks=pytest.mark.xfail(raises=TypeError, strict=True)),
+
+    (Normal1, ASingleton, Interface),
+    (Normal1, Interface, ASingleton),
+    (ASingleton, Normal1, Interface),
+
+    pytest.param((Normal1, AWidget, Interface),
+                 marks=pytest.mark.xfail(raises=TypeError, strict=True)),
+    (Normal1, Interface, AWidget),
+    pytest.param((AWidget, Normal1, Interface),
+                 marks=pytest.mark.xfail(raises=TypeError, strict=True)),
+
+    (Normal1, ASingleton, AWidget),
+    (Normal1, AWidget, ASingleton),
+    (ASingleton, Normal1, AWidget),
+    (ASingleton, AWidget, Normal1),
+    (AWidget, Normal1, ASingleton),
+    (AWidget, ASingleton, Normal1),
+
+    (Normal1, Interface, ASingleton, AWidget),
+    (Normal1, Interface, AWidget, ASingleton),
+    (ASingleton, Normal1, Interface, AWidget),
+    pytest.param((AWidget, Normal1, Interface, ASingleton),
+                 marks=pytest.mark.xfail(raises=TypeError, strict=True)),
+    pytest.param((ASingleton, AWidget, Normal1, Interface),
+                 marks=pytest.mark.xfail(raises=TypeError, strict=True)),
+    pytest.param((AWidget, ASingleton, Normal1, Interface),
+                 marks=pytest.mark.xfail(raises=TypeError, strict=True)),
+    (Normal1, ASingleton, Interface, AWidget),
+    pytest.param((Normal1, AWidget, Interface, ASingleton),
+                 marks=pytest.mark.xfail(raises=TypeError, strict=True)),
+    pytest.param((ASingleton, Normal1, AWidget, Interface),
+                 marks=pytest.mark.xfail(raises=TypeError, strict=True)),
+    pytest.param((AWidget, Normal1, ASingleton, Interface),
+                 marks=pytest.mark.xfail(raises=TypeError, strict=True)),
+]
+
+
+@pytest.mark.parametrize('subclasses', SUBCLASSES_TESTS)
+def test_subclasses_direct(subclasses: Iterable[Type]) -> None:
+    class ToTest(MetaClassResolver(*subclasses)):  # type: ignore[misc]
+
+        def method2(self) -> bool:
+            return True
+
+        def method3(self) -> bool:
+            '''
+            Even method3 is supposed to be implemented by a subclass,
+            for tests to work properly we still need to implement it.
+            Otherwise some tests would fail because the class is still abstract.
+            '''
+            return True
+
+    check(ToTest, subclasses)
+
+
+@pytest.mark.parametrize('subclasses', SUBCLASSES_TESTS)
+def test_subclasses_subclass(subclasses: Iterable[Type]) -> None:
+    '''Separate subclass tests such that the xfail ones also run for the subclass case'''
+
+    class ToTest(MetaClassResolver(*subclasses)):  # type: ignore[misc]
+
+        def method2(self) -> bool:
+            return True
+
+    class SubclassToTest(ToTest):
+
+        def method3(self) -> bool:
+            return True
+
+    check(SubclassToTest, subclasses, check_method3=True)
