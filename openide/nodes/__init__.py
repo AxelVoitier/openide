240352915ac4aa0033ea71aaad8d1bd5325e1c25
Axel Voitier <axel.voitier@gmail.com>
2025-09-21 15:03:02 +0200

    AbstractSheetUser: Was missing the import __all__ in __init__.py

--- openide/nodes/__init__.py
+++ openide/nodes/__init__.py
@@ -9,14 +9,16 @@ from __future__ import annotations
 # Initially we are mostly going to re-export from _like_netbeans.
 # And as it progress towards a more pythonic interfaces, the various original
 #  bits will be replaced with newer from here.
-from . import _like_netbeans, properties, properties_support, sheet_support
+from . import _like_netbeans, abstract_sheet_user, properties, properties_support, sheet_support
 from ._like_netbeans import *  # noqa: F403
+from .abstract_sheet_user import *  # noqa: F403
 from .properties import *  # noqa: F403
 from .properties_support import *  # noqa: F403
 from .sheet_support import *  # noqa: F403
 
 __all__: list[str] = []
 __all__ += _like_netbeans.__all__
+__all__ += abstract_sheet_user.__all__
 __all__ += properties.__all__
 __all__ += properties_support.__all__
 __all__ += sheet_support.__all__
