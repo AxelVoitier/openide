2f3f7faeaa861b27080fbad9fefe67aa686df9e9
Axel Voitier <axel.voitier@gmail.com>
2025-09-21 15:03:45 +0200

    Explorer/PropertySheet: Removing import __all__ for dead/prototype code

--- openide/explorer/property_sheet/__init__.py
+++ openide/explorer/property_sheet/__init__.py
@@ -8,23 +8,14 @@ from __future__ import annotations
 
 from . import (
     editor_support,
-    property_set_model,
-    property_sheet,
     sheet_model,
-    sheet_model_table,
     sheet_table,
 )
 from .editor_support import *  # noqa: F403
-from .property_set_model import *  # noqa: F403
-from .property_sheet import *  # noqa: F403
 from .sheet_model import *  # noqa: F403
-from .sheet_model_table import *  # noqa: F403
 from .sheet_table import *  # noqa: F403
 
 __all__: list[str] = []
 __all__ += editor_support.__all__
-__all__ += property_set_model.__all__
-__all__ += property_sheet.__all__
 __all__ += sheet_model.__all__
-__all__ += sheet_model_table.__all__
 __all__ += sheet_table.__all__
